fc32782a9cd1034405a9322ab68ef14e507e5e39
Sannidhi-Shetty2 <sannidhi.r.v@gmail.com>
2020-03-26 15:43:36 +0530

    This is .net application

---
+++ GitTest/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            Console.WriteLine("Enter your name:");
+            username = Console.ReadLine();
+            Console.WriteLine("Hi,{0} \nWelcome to GITHUB Tutorial", username);
+            Console.ReadLine();
+        }
+    }
+}
